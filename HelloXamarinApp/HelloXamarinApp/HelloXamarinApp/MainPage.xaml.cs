47c2608ad36a45c7e6aeb4d435af1c12d2ffeed3
QuangNB <basquang@gmail.com>
2019-05-15 11:15:10 +0700

    Hello Xamarin App

---
+++ HelloXamarinApp/HelloXamarinApp/HelloXamarinApp/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HelloXamarinApp
+{
+    public partial class MainPage : ContentPage
+    {
+        string _fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "notes.txt");
+        public MainPage()
+        {
+            InitializeComponent();
+
+            if (File.Exists(_fileName))
+            {
+                editor.Text = File.ReadAllText(_fileName);
+            }
+
+            btnSave.Clicked += BtnSave_Clicked;
+            btnDelete.Clicked += BtnDelete_Clicked;
+        }
+
+        private void BtnDelete_Clicked(object sender, EventArgs e)
+        {
+            if (File.Exists(_fileName))
+            {
+                File.Delete(_fileName);
+            }
+            editor.Text = string.Empty;
+        }
+
+        private void BtnSave_Clicked(object sender, EventArgs e)
+        {
+            File.WriteAllText(_fileName, editor.Text);
+        }
+    }
+}
