406cb982562e9d892a2d0822cfdc889b3dca4eaf
QuangNB <basquang@gmail.com>
2019-05-21 08:26:56 +0700

    Carousel page like Pivot control UWP

---
+++ CarouselPivotApp/CarouselPivotApp/CarouselPivotApp/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CarouselPivotApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class MainPage : CarouselPage
+    {
+        ObservableCollection<MyClass> AllDays = new ObservableCollection<MyClass>();
+        public MainPage()
+        {
+            InitializeComponent();
+            //AllDays.Add(new MyClass { MyProperty = DateTime.Now.AddDays(-1) });
+            AllDays.Add(new MyClass { MyProperty = DateTime.Now });
+            AllDays.Add(new MyClass { MyProperty = DateTime.Now.AddDays(1) });
+            CurrentPageChanged += MainPage_CurrentPageChanged;
+            ItemsSource = AllDays;
+        }
+
+        private void MainPage_CurrentPageChanged(object sender, EventArgs e)
+        {
+            var currentIndex = Children.IndexOf(CurrentPage);
+
+            if (currentIndex == 0)
+            {
+                AllDays.Insert(0, new MyClass { MyProperty = AllDays[currentIndex].MyProperty.AddDays(-1) });
+            }
+            if (currentIndex == Children.Count - 1)
+            {
+                AllDays.Add(new MyClass { MyProperty = AllDays[currentIndex].MyProperty.AddDays(1) });
+            }
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();            
+            CurrentPage = Children[1];
+        }
+    }
+    public class MyClass
+    {
+        public DateTime MyProperty { get; set; }
+    }
+}
